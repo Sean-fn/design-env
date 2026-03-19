d706e9ff0a365e5c1c25e3942fcae56a6604c1fe
Sean-fn <seanfang122@gmail.com>
2026-03-19 15:25:14 +0800

    update

--- test-delete/src/components/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Dashboard() {
-  return (
-    <div className="flex flex-col w-full h-full">
-      {/* Empty Dashboard as requested */}
-    </div>
-  );
-}
